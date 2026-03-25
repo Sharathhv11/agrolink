d514ebab7fb9b5d7150cf80b59315750224c1679
Sharath <sharathhv88@gmail.com>
2026-03-26 01:06:15 +0530

    ui improvization/

--- server/services/laborerNotificationService.js
+++ server/services/laborerNotificationService.js
@@ -1,8 +1,8 @@
 const twilio = require("twilio");
 
 
-// const accountSid = "";
-// const authToken = "";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 // 📲 Create client
 const client = twilio(accountSid, authToken);
